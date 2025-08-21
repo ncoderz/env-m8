3ae552bd6421eaeb77fe5681bdcafdbe2949057b
six5536 <10079653+six5536@users.noreply.github.com>
2025-08-21 13:56:25 +1200

    fix: fixed bug in simple version parsing

--- src/EnvM8Utils.ts
+++ src/EnvM8Utils.ts
@@ -13,19 +13,19 @@ class EnvM8Utils {
     }
 
     for (let i = 0; i < 2; i++) {
-      let verRegex: RegExp;
+      let v: RegExpMatchArray | string[] | null;
       const semVer = i === 0;
       if (semVer) {
         // Lenient Semver 2.0 regex, lazy init and cached
-        verRegex = SEM_VER_REGEX ??=
-          /^([0-9a-zA-Z]+)\.([0-9a-zA-Z]+)\.([0-9a-zA-Z]+)(?:-([0-9a-zA-Z\.-]+))?(?:\+([0-9a-zA-Z\.-]+))?$/;
+        const regex = (SEM_VER_REGEX ??=
+          /^([0-9a-zA-Z]+)\.([0-9a-zA-Z]+)\.([0-9a-zA-Z]+)(?:-([0-9a-zA-Z\.-]+))?(?:\+([0-9a-zA-Z\.-]+))?$/);
+        v = version.match(regex);
       } else {
-        // Simple version regex, lazy init and cached
-        // Matches X.Y.Z or X.Y.Z.B or X.Y.Z-B or X.Y or X-Y.Z+B, etc
-        verRegex = SIMPLE_VER_REGEX ??= /^([A-Za-z0-9]+(?:[._\-+][A-Za-z0-9]+){0,3})$/;
+        const regex = (SIMPLE_VER_REGEX ??= /[._\-+]/);
+        // Simple version, major.minor.patch.build where everything is optional
+        v = ['', ...version.split(regex, 4)];
       }
 
-      const v = version.match(verRegex);
       if (v) {
         return {
           full: version,
